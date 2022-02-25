0c4de2180efd2903401b21e156248603c009c7dc
SnackTraces <code@snacktraces.com>
2022-02-24 22:31:05 -0500

    Personalized example

---
+++ craigmath/mathmain.py
@@ -0,0 +1,11 @@
+def add(x, y):
+    return x + y
+
+
+def sub(x, y):
+    return x - y
+
+
+if __name__ == "__main__":
+    """This runs when you execute 'python3 craigmath/mathmain.py'"""
+    my_function()
